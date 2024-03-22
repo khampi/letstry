50a31189d9b96bbcda224244fa3e632317ed08cb
khampi <118457088+khampi@users.noreply.github.com>
2024-03-22 18:44:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const btn1 = document.getElementById("btn1")
+btn1.onmouseover = function(){
+    btn1.innerHTML="Thanks is not Required😒 ,Waiting for Party";
+
+}
+btn1.onmouseout = function(){
+    btn1.innerHTML="Thank you Hampi";
+}
+const btn2 = document.getElementById("btn2");
+
+btn2.onclick = function(){
+    document.body.style.backgroundImage = "url('links/img4.jpeg')";
+    document.body.style.backgroundSize = 50% auto ;
+
+
+    
+}
+    
